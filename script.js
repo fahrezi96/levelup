fe3cc243989f15c238f96f8a99277045f1ff8279
fahrezi96 <ifmuhamad@gmail.com>
2024-03-28 11:11:20 +0700

    update dasboard

--- script.js
+++ script.js
@@ -19,6 +19,16 @@ navLink.forEach((link) => {
   });
 });
 
+// if (window.innerWidth <= 1080) {
+//   aside.classList.add('close');
+// }
+
+// window.addEventListener('resize', () => {
+//   if (window.innerWidth <= 1080) {
+//     aside.classList.add('close');
+//   }
+// });
+
 // CHART
 window.onload = function () {
   const chart1 = new CanvasJS.Chart('chart1', {
@@ -26,6 +36,9 @@ window.onload = function () {
     theme: 'light2',
     title: {
       text: 'Top 10 Receivable',
+    },
+    axisY: {
+      gridDashType: 'dash',
     },
     data: [
       {
@@ -49,6 +62,9 @@ window.onload = function () {
     theme: 'light2',
     title: {
       text: 'Top 10 Payable',
+    },
+    axisY: {
+      gridDashType: 'dash',
     },
     data: [
       {
@@ -87,4 +103,135 @@ window.onload = function () {
     ],
   });
   chart3.render();
+
+  var chart4 = new CanvasJS.Chart('chart4', {
+    animationEnabled: true,
+    theme: 'light2',
+    title: {
+      text: 'Balance Sheet 2024',
+    },
+    legend: {
+      cursor: 'pointer',
+      itemclick: toggleDataSeries,
+    },
+    axisY: {
+      title: 'Value',
+      gridDashType: 'dash',
+    },
+    axisY2: {
+      title: '%',
+      titleFontColor: '#019B73',
+      lineColor: '#019B73',
+      tickColor: '#019B73',
+      labelFontColor: '#019B73',
+    },
+    data: [
+      {
+        type: 'column',
+        name: 'Asset',
+        showInLegend: true,
+        legendMarkerColor: '#9D75CB',
+        dataPoints: [
+          { label: 'Jan', y: 230000000, color: '#9D75CB' },
+          { label: 'Feb', y: 230000000, color: '#9D75CB' },
+          { label: 'Mar', y: 500000000, color: '#9D75CB' },
+        ],
+      },
+      {
+        type: 'column',
+        name: 'Liabilities',
+        showInLegend: true,
+        legendMarkerColor: '#FF9F1C',
+        dataPoints: [
+          { label: 'Jan', y: 80000000, color: '#FF9F1C' },
+          { label: 'Feb', y: 80000000, color: '#FF9F1C' },
+          { label: 'Mar', y: 100000000, color: '#FF9F1C' },
+        ],
+      },
+      {
+        type: 'spline',
+        name: 'Debt Equity Ratio',
+        axisYType: 'secondary',
+        showInLegend: true,
+        legendMarkerColor: '#06D6A0',
+        lineColor: '#06D6A0',
+        dataPoints: [
+          { label: 'Jan', y: 25, color: '#06D6A0' },
+          { label: 'Feb', y: 25, color: '#06D6A0' },
+          { label: 'Mar', y: 15, color: '#06D6A0' },
+        ],
+      },
+    ],
+  });
+  chart4.render();
+
+  var chart5 = new CanvasJS.Chart('chart5', {
+    animationEnabled: true,
+    theme: 'light2',
+    title: {
+      text: 'Income Statement 2024',
+    },
+    legend: {
+      cursor: 'pointer',
+      itemclick: toggleDataSeries,
+    },
+    axisY: {
+      title: 'Value',
+      gridDashType: 'dash',
+    },
+    axisY2: {
+      title: '%',
+      titleFontColor: '#019B73',
+      lineColor: '#019B73',
+      tickColor: '#019B73',
+      labelFontColor: '#019B73',
+    },
+    data: [
+      {
+        type: 'column',
+        name: 'Revenue',
+        showInLegend: true,
+        legendMarkerColor: '#9D75CB',
+        dataPoints: [
+          { label: 'Jan', y: 0, color: '#9D75CB' },
+          { label: 'Feb', y: 0, color: '#9D75CB' },
+          { label: 'Mar', y: 200000000, color: '#9D75CB' },
+        ],
+      },
+      {
+        type: 'column',
+        name: 'Net Income',
+        showInLegend: true,
+        legendMarkerColor: '#FF9F1C',
+        dataPoints: [
+          { label: 'Jan', y: 0, color: '#FF9F1C' },
+          { label: 'Feb', y: 0, color: '#FF9F1C' },
+          { label: 'Mar', y: 200000000, color: '#FF9F1C' },
+        ],
+      },
+      {
+        type: 'spline',
+        name: 'Margin',
+        axisYType: 'secondary',
+        showInLegend: true,
+        legendMarkerColor: '#06D6A0',
+        lineColor: '#06D6A0',
+        dataPoints: [
+          { label: 'Jan', y: 100, color: '#06D6A0' },
+          { label: 'Feb', y: 0, color: '#06D6A0' },
+          { label: 'Mar', y: 100, color: '#06D6A0' },
+        ],
+      },
+    ],
+  });
+  chart5.render();
+
+  function toggleDataSeries(e) {
+    if (typeof e.dataSeries.visible === 'undefined' || e.dataSeries.visible) {
+      e.dataSeries.visible = false;
+    } else {
+      e.dataSeries.visible = true;
+    }
+    e.chart.render();
+  }
 };
